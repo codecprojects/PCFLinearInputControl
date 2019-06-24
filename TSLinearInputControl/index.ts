c7765eab08dbe6049293a2f44e2498293f88e51f
S00083446 <S00083446@mail.itsligo.ie>
2019-06-24 15:32:24 +0100

    changed maximum to 130 to be used for age field

--- TSLinearInputControl/index.ts
+++ TSLinearInputControl/index.ts
@@ -40,7 +40,7 @@ public init(context: ComponentFramework.Context<IInputs>, notifyOutputChanged: (
 	this.inputElement.addEventListener("input",this._refreshData);
 	//setting the max and min values for the control.
 	this.inputElement.setAttribute("min","1");
-	this.inputElement.setAttribute("max","1000");
+	this.inputElement.setAttribute("max","130");
 	this.inputElement.setAttribute("class","linearslider");
 	this.inputElement.setAttribute("id","linearrangeinput");
 	// creating a HTML label element that shows the value that is set on the linear range control
